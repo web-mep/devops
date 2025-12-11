eef25e636acf3f302da5c9e4144940f97119d503
Sonu KR <205268823+web-mep@users.noreply.github.com>
2025-12-11 05:57:56 +0000

    get-user-landonpage

---
+++ Basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 123456;
+let accountEmail = "sonukumar@madeeasy.in";
+var accountPassword = 123456;
+accountCity = "New Delhi"; // Implicit global variable
+
+//accountId = 2 not allowed to change because it is declared using const
+
+accountEmail = "sk@madeeasy.in"
+accountPassword = "123456789"
+accountCity = "Noida"
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]);
+
+
