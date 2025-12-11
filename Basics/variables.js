7ecec2e55c4f5a8dac75ebab4b496fdb09c719f9
Sonu KR <205268823+web-mep@users.noreply.github.com>
2025-12-11 06:10:54 +0000

    email-change

--- Basics/variables.js
+++ Basics/variables.js
@@ -1,5 +1,5 @@
 const accountId = 123456;
-let accountEmail = "sonukumar@madeeasy.in";
+let accountEmail = "test@devops.in";
 var accountPassword = 123456;
 accountCity = "New Delhi"; // Implicit global variable
 
@@ -8,8 +8,9 @@ accountCity = "New Delhi"; // Implicit global variable
 accountEmail = "sk@madeeasy.in"
 accountPassword = "123456789"
 accountCity = "Noida"
+accountState = "Uttar Pradesh" // Implicit global variable  
 
 console.log(accountId);
-console.table([accountEmail, accountPassword, accountCity]);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
 
 
